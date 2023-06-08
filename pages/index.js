85038ad03d304c2f4456ee9361732a8f07846663
Mubasshir Ahmed <lastmubasshir@gmail.com>
2023-06-08 15:34:35 +0600

    main home heading added

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,23 @@
 import HeroBanner from 'componemt/components/HeroBanner';
+import Wrapper from 'componemt/components/Wrapper';
 
 export default function Home() {
   return (
     <>
       <main className="h-[3000px]">
         <HeroBanner />
+        <Wrapper>
+          <div className="mx-auto my-[50px] max-w-[800px] text-center md:my-[80px]">
+            <div className="mb-5 text-[28px] font-semibold leading-tight md:text-[34px]">
+              Cushioning for Your Miles
+            </div>
+            <div className="text-md md:text-xl">
+              A lightweight Nike ZoomX midsole is combined with increased stack
+              heights to help provide cushioning during extended stretches of
+              running.
+            </div>
+          </div>
+        </Wrapper>
       </main>
     </>
   );
